45257a9caa97a56d286efc3841a1d553590d2e19
picrin <Adam Kurkiewicz>
2019-03-25 16:04:45 +0000

    Reinstatiating the original ldap example

---
+++ ldap/ldap_example_UG.py
@@ -0,0 +1,8 @@
+import ldap
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
+pprint = pp.pprint
+l = ldap.initialize("ldap://spitfire.campus.gla.ac.uk")
+l.simple_bind_s("2333134p@campus.gla.ac.uk", "<Marcell's password>")
+result = l.search_s("CN=Person,CN=Schema,CN=Configuration,DC=campus,DC=gla,DC=ac,DC=uk", ldap.SCOPE_SUBTREE, "(cn=2333134P)")
+print(result)
