c5b8b2abc3f66dfdf9d830b760358ee59233a6e8
Marcell Pek <marcellpek0@gmail.com>
2019-03-26 14:44:52 +0000

    Add nohup to python3

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source /home/ubuntu/attendanceltc-service-virtualenv/bin/activate
-python3 /home/ubuntu/attendanceltc-service/run.py
+nohup python3 /home/ubuntu/attendanceltc-service/run.py &
