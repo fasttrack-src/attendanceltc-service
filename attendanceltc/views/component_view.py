796b2828302bce67bf88091075c9e9407710fda1
Iva Babukova <iva.babukova@skyscanner.net>
2019-06-17 15:21:56 +0100

    started working on the second table, but I have a bug in the python part that displays no attendance

--- attendanceltc/views/component_view.py
+++ attendanceltc/views/component_view.py
@@ -36,7 +36,7 @@ def view_attendance_for_component(component, subject_id, catalog_id):
     tier4_students = [s for s in students if s.tier4]
 
     attendance = db.session.query(Attendance) \
-        .with_entities(Attendance.timestamp) \
+        .with_entities(Attendance.timestamp, Attendance.student_id) \
         .filter(Attendance.student_id.in_((s.id for s in students))) \
         .all()
 
@@ -46,14 +46,50 @@ def view_attendance_for_component(component, subject_id, catalog_id):
 
     for a in attendance:
         ts = a.timestamp
-        print("&&&", ts)
         for i in range(0, len(raw_weeks)):
             if raw_weeks[i][0] <= ts and raw_weeks[i][1] >= ts:
                 weekly_attendance_count[i] += 1
-                print(ts, "yes", raw_weeks[i][0], raw_weeks[i][1])
                 break
 
-    print(weekly_attendance_count)
+    # Get beginning of last weekday.
+    today = datetime.date.today()
+    start_of_last_weekday = datetime.timedelta(days=today.weekday(), weeks=1)
+    start_of_last_weekday = today - start_of_last_weekday
+    two_weeks_ago = today - datetime.timedelta(days=14)
 
 
-    return render_template("component_view.html", weeks=week_labels, attendance_per_week=weekly_attendance_count)
+    # TODO This is buggy
+    student_attendance = []
+    for s in students:
+        attendance_data = [a.timestamp for a in attendance if a.student_id == s.id]
+        student_attendance.append([
+            s.firstname + " " + s.lastname,
+            str(len(attendance_data)) + "/" + str(len(raw_weeks)),
+            has_attended(attendance_data, start_of_last_weekday),
+            has_attended(attendance_data, two_weeks_ago, start_of_last_weekday)
+        ])
+
+    result = OrderedDict()
+    for i in range(0, len(students)):
+        result[i] = student_attendance[i]
+
+    print("^%%&", result)
+
+    context = {
+        "weeks": week_labels,
+        "attendance_per_week": weekly_attendance_count,
+        "components": result
+    }
+
+    return render_template(
+        "component_view.html",
+        **context
+    )
+
+
+def has_attended(attendance_dates, begin_week, end_week=None):
+    for i in attendance_dates:
+        if i > begin_week and (i < end_week or not end_week):
+            return True
+
+    return False
